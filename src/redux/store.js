551292a9d8011197d4dcc335af9858ea8f8e23d9
sudheer kumar <sudheerkvy1996@gmail.com>
2020-09-10 08:12:58 +0530

    small changes made in store.js, removed redux-logger from producion build

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,11 @@ import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import {persistStore } from 'redux-persist';
-const middleWares = [logger] ;//array
+const middleWares = [] ;//array
 
 if(process.env.NODE_ENV === 'development'){
    middleWares.push(logger);
-}
+}//it works when we use local server to run project middleware push the logger 
 
 export const store = createStore(rootReducer,applyMiddleware(...middleWares));
 export const persistor = persistStore(store);
